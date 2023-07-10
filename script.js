9f69ad570a8d71f5383c5e861c988942e838bf43
David Oliveira <60549321+DOliver14@users.noreply.github.com>
2023-07-10 14:09:14 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() { //funcao toglle mode
+    const html = document.documentElement 
+    html.classList.toggle('light')
+
+  //pegar a tag img
+
+  const img = document.querySelector(".profile img")
+    
+  //substituir img
+    if(html.classList.contains('light')){
+        //se tiver light mode , adicionar a imagem light
+        img.setAttribute('src', 'aula1/image/avatarlight.png')
+    }else{
+        //se tiver sem light mode , manter img normal
+        img.setAttribute('src', 'aula1/image/fotonight.png')
+    }
+    
+
+
+
+
+}
+
